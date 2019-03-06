b902df7ce7844940cc5d5b786d91621a1de094e8
Colby Hemond <colbyhemond@Colbys-MacBook-Pro.local>
2019-03-05 20:31:50 -0500

    add client/built to express app.js for react part of app

--- app.js
+++ app.js
@@ -24,6 +24,10 @@ app.use('/', indexRouter);
 app.use('/emails', emailsRouter);
 app.use('/services', servicesRouter);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
